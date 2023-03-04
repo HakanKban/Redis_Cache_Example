9be9fe8951a43ae0bcb6a947f9cb288aaffb6a63
Hakan Kaban <90520449+HakanKban@users.noreply.github.com>
2023-03-04 18:26:38 +0300

    Add project files.

---
+++ Distributed.Caching/Controllers/ValuesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text;
+
+namespace Distributed.Caching.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ValuesController : ControllerBase
+{
+    readonly IDistributedCache _distributedCache;
+
+    public ValuesController(IDistributedCache distributedCache)
+    {
+        _distributedCache = distributedCache;
+    }
+
+    [HttpGet("set")]
+
+    public async Task<IActionResult> set(string Name,string surName)
+    {
+        await _distributedCache.SetStringAsync(Name, surName);
+        await _distributedCache.SetAsync("surName", Encoding.UTF8.GetBytes(surName));
+        return Ok();
+    }
+  
+    [HttpGet("get")]
+
+    public async Task<IActionResult> get()
+    {
+        var name = await _distributedCache.GetStringAsync("name");
+        var surnameBinary = await _distributedCache.GetAsync("name");
+        var surName = Encoding.UTF8.GetString(surnameBinary);
+        return Ok(new
+        {
+            name,
+            surName
+        });
+    }
+
+}
---
+++ InMemory.Caching/Controllers/ValuesController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemory.Caching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        readonly IMemoryCache _memoryCache;
+
+        public ValuesController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        //AbsolueTime => cache'lenen datanın kesin olarak ömrünü belirtilmesidir.
+        //SlidingTime => Belirtilen süre zarfında veriye erişim yapıldığında ömrü bir o kadar uzar. İki zamanlama modeli beraber kullanılabilir.
+
+        [HttpGet("set/{name}")]
+        public void SetName(string name)
+        {
+            _memoryCache.Set("Name", name);
+        }
+
+        [HttpGet]
+        public string? GetName()
+        {
+           return _memoryCache.Get<string>("Name");
+        }
+
+
+        [HttpGet]
+        public void SetDate() 
+        {
+            _memoryCache.Set<DateTime>("date", DateTime.Now, options: new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+                SlidingExpiration = TimeSpan.FromSeconds(5)
+            });
+        }
+
+        [HttpGet("getDate")]
+        public DateTime GetDate()
+        {
+            return _memoryCache.Get<DateTime>("date");
+        }
+
+
+    }
+}
